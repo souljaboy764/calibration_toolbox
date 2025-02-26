a548916ce179e0c58b5d63123fbe0a46103cad04
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2025-02-26 16:44:02 +0100

    added argparse option for calibration type

--- calibration_toolbox/calibration.py
+++ calibration_toolbox/calibration.py
@@ -1,10 +1,12 @@
 from calibration_toolbox.axxb import AXXBCalibrator
 import argparse
 
-AXXBCalib = AXXBCalibrator('EBCB')
 parser = argparse.ArgumentParser(description='Calibration Node')
 parser.add_argument('--data_path', type=str, required=True, help='Path to save calibration data')
+parser.add_argument('--calib_type', type=str, choices=['EBCB', 'EBME', 'EH'], required=True, help='Type of calibration')
 args = parser.parse_args()
+
+AXXBCalib = AXXBCalibrator(args.calib_type)
 AXXBCalib.load_xforms(args.data_path)
 
 cam2ee = AXXBCalib.axxb()
